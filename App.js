a03f0166a0cadff356de8740ec84f2b38fb1cf16
chloecookie <76987180+chloecookie@users.noreply.github.com>
2022-11-27 13:20:42 -0800

    Update App.js

--- App.js
+++ App.js
@@ -1,28 +1,28 @@
 import React, { useState, useEffect } from "react";
 import moment from "moment";
 import Leftside from "./Leftside";
-import Search from "./Search";
 import Rightside from "./Rightside";
 import { v4 as uuidv4 } from "uuid";
 import { ChakraProvider } from "@chakra-ui/react";
 import "./App.css";
 import axios from "axios";
-import { IconButton } from "@chakra-ui/react";
-import { AddIcon } from "@chakra-ui/icons";
+// import { IconButton } from "@chakra-ui/react";
+// import { AddIcon } from "@chakra-ui/icons";
 
 const App = () => {
+  const [notes, setNotes] = useState([
+    {
+      id: "",
+      title: "",
+      task: "",
+      date: "",
+      completed: false,
+    },
+  ]);
 
-  const [notes, setNotes] = useState({
-    id: "",
-    title: "",
-    task: "",
-    completed: false 
-  });
-  
   const [selectedTag, setSelectedTag] = useState("");
-  const [dailyDate, setDailyDate] = useState("yyyy-mm-dd");
   const getNotes = async () => {
-    const response = await fetch(" http://localhost:3001/note", {
+    const response = await fetch(" http://localhost:3001/notes", {
       method: "GET",
     });
     const data = await response.json();
@@ -35,31 +35,37 @@ const App = () => {
 
   // when data arrives from the server, the data gets stored into the state using the function setTitle
 
-  const Date = () => {
-    return (
-      <div className="daily">
-        <p>What date it is today?</p>
-        <input
-          type="date"
-          id="start"
-          name="trip-start"
-          value={dailyDate}
-          min="2000-11-16"
-          max="2030-11-16"
-          onChange={(e) => setDailyDate(e.target.value)}
-        ></input>
-      </div>
-    );
-  };
   const updateNotes = (newNote) => {
     setNotes([...notes, newNote]);
+  };
+
+  const onChangeToggle = async (tag) => {
+    setSelectedTag(tag);
+
+    const response = await fetch("http://localhost:3001/notes", {
+      method: "GET",
+    });
+    var data = await response.json();
+    const todayDateFormated = moment().format("MM/DD/YYYY");
+
+    if (selectedTag === "today") {
+      data = data.filter((note) => note.date === todayDateFormated);
+    } else if (selectedTag === "next 7 days") {
+      data = data.filter((note) => {
+        const todoDate = moment(note.date, "MM/DD/YYYY");
+        const diffDays = todoDate.diff(todayDateFormated, "days");
+        console.log(diffDays);
+        return diffDays >= 0 && diffDays < 7;
+      });
+    } else if (selectedTag === "all tasks") {
+      setNotes(data);
+    }
   };
 
   const Toggle = () => {
     return (
       <div>
-        <select onClick={(e) => setSelectedTag(e.target.value)}>
-          console.log(selectedTag)
+        <select onChange={(e) => onChangeToggle(e.target.value)}>
           <option value="today">Today</option>
           <option value="next 7 days">Next 7 Days</option>
           <option value="all tasks">All Tasks</option>
@@ -68,50 +74,23 @@ const App = () => {
     );
   };
 
-  const useFilterTodos = (note, selectedTag) => {
-    const [filteredTodos, setFilteredTodos] = useState([]);
-
-    useEffect(() => {
-      console.log(selectedTag);
-      let data;
-      const todayDateFormated = moment().format("MM/DD/YYYY");
-
-      if (selectedTag === "today") {
-        data = note.filter((not) => not.date === todayDateFormated);
-      } else if (selectedTag === "next 7 days") {
-        data = note.filter((not) => {
-          const todoDate = moment(note.date, "MM/DD/YYYY");
-          const todayDate = moment(todayDateFormated, "MM/DD/YYYY");
-
-          const diffDays = todoDate.diff(todayDate, "days");
-
-          return diffDays >= 0 && diffDays < 7;
-        });
-      } else if (selectedTag === "all days") {
-        data = note;
-      }
-    }, [selectedTag]);
-  };
-
   return (
-    // <ChakraProvider>
-    <div>
-      <h2> Good morning, Chloe ☀️</h2>
-      <p> Here's the plan for today</p>
-      <Rightside updateNotes={updateNotes} />
-      <IconButton
-        className="add"
-        aria-label="Search database"
-        icon={<AddIcon />}
-        size="xs"
-      />
-      <Leftside notes={notes} />
-      <Search />
-      <Toggle />
-      <Date />
-    </div>
-    // </ChakraProvider>
+    <ChakraProvider>
+      <div>
+        <h2> Good morning, Chloe ☀️</h2>
+        <p> Here's the plan for today</p>
+        <Rightside updateNotes={updateNotes} />
+        {/* <IconButton
+          className="add"
+          aria-label="Search database"
+          icon={<AddIcon />}
+          size="xs"
+        /> */}
+        <Leftside notes={notes} />
+        <Toggle />
+      </div>
+    </ChakraProvider>
   );
 };
-
 export default App;
+

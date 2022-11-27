bb293f5d7d483e6181a85dd939715547d1f4d23b
chloecookie <76987180+chloecookie@users.noreply.github.com>
2022-11-27 13:21:10 -0800

    Update Leftside.js

--- Leftside.js
+++ Leftside.js
@@ -1,20 +1,13 @@
 import React, { useState } from "react";
 import Rightside from "./Rightside";
 import { v4 as uuidv4 } from "uuid";
-import { toVarDefinition } from "@chakra-ui/react";
 
 const LeftSide = ({ notes }) => {
-  console.log(notes);
   return (
     <div className="left">
       {notes.map((n) => (
-        <div key={n.id} style={{display: "flex"}}>
-          <input type="checkbox" />
-          <h2
-          style={{
-              color: "white",
-              textDecoration:toVarDefinition.comple
-          }}>{n.title}</h2>
+        <div key={n.id}>
+          <h2>{n.title}</h2>
           <p>{n.task.substring(0, 20)}</p>
         </div>
       ))}
@@ -22,3 +15,18 @@ const LeftSide = ({ notes }) => {
   );
 };
 export default LeftSide;
+
+const Leftside = ({ notes }) => {
+  useEffect(() => {
+    return (
+      <div className="left">
+        {notes.map((n) => (
+          <div key={n.id}>
+            <h2>{n.title}</h2>
+            <p>{n.task.substring(0, 20)}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }, []);
+};

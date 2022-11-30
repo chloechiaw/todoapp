93b34661545d4778460485bae540c16dde8dae55
chloecookie <76987180+chloecookie@users.noreply.github.com>
2022-11-30 15:06:57 -0800

    Update Rightside.js

--- Rightside.js
+++ Rightside.js
@@ -1,19 +1,23 @@
 import React, { useState, useEffect } from "react";
+import moment from "moment";
+
 import Leftside from "./Leftside";
 import "./App.css";
 // import { IconButton } from "@chakra-ui/react";
 // import { AddIcon } from "@chakra-ui/icons";
 
-const Rightside = ({ updateNotes }) => {
+const todayDateFormated = moment().format("YYYY/MM/DD");
+
+const Rightside = ({ updateNotes, notes, todayDateFormated }) => {
   const [title, setTitle] = useState("");
   const [task, setTask] = useState("");
-  const [date, setDate] = useState(["yyyy-mm-dd"]);
+  const [date, setDate] = useState([{ todayDateFormated }]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     const note = { title, task, date };
     console.log(note);
-    const response = await fetch("http://localhost:3001/notes", {
+    const response = await fetch("http://localhost:3003/notes", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(note),
@@ -25,42 +29,51 @@ const Rightside = ({ updateNotes }) => {
   };
 
   return (
-    <div className="right">
-      <form onSubmit={handleSubmit} className="Outline">
-        <input
-          className="taskfont textareawidth "
-          type="text"
-          placeholder="Task"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        ></input>
-        <br></br>
-        <br></br>
-        <input
-          className="datefont"
-          type="date"
-          id="start"
-          name="trip-start"
-          value={date}
-          min="2022-11-19"
-          max="2030-11-16"
-          onChange={(e) => setDate(e.target.value)}
-        ></input>
-        <br></br>
-        <br></br>
-        <textarea
-          className="textareawidth"
-          placeholder="Write..."
-          rows="20"
-          value={task}
-          onChange={(e) => setTask(e.target.value)}
-        ></textarea>
-        <br></br>
-        <button onClick={handleSubmit}>Add</button>
-      </form>
+    <div className="w-screen h-screen grid grid-rows-2 text-1xl md:grid-cols-2">
+      <div className="w-full h-full md:h-screen">
+        <form onSubmit={handleSubmit} className="Outline">
+          <input
+            className="taskfont textareawidth "
+            type="text"
+            placeholder="Task"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          ></input>
+          <br></br>
+          <br></br>
+          <input
+            className="datefont"
+            type="date"
+            id="start"
+            name="trip-start"
+            value={date}
+            min="2022-11-19"
+            max="2030-11-16"
+            onChange={(e) => setDate(e.target.value)}
+          ></input>
+          <br></br>
+          <br></br>
+          <textarea
+            className="textareawidth"
+            placeholder="Write..."
+            rows="20"
+            value={task}
+            onChange={(e) => setTask(e.target.value)}
+          ></textarea>
+          <br></br>
+          <button onClick={handleSubmit}>Add</button>
+        </form>
+      </div>
+      <div>
+        {notes.map((n) => (
+          <div key={n.id}>
+            <h2>{n.title}</h2>
+            <p>{n.task.substring(0, 20)}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
 
 export default Rightside;
-
